505447cbc31899d2c583b7588fd6adc00c510c63
Hoang Nguyen <hoang.nguyen@stunited.vn>
2025-08-15 14:41:52 +0700

    refactor: remove unused imports and methods

--- src/modules/workspace-user/workspace-user.controller.ts
+++ src/modules/workspace-user/workspace-user.controller.ts
@@ -1,7 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { WorkspaceUserService } from './workspace-user.service';
 
 @Controller('workspace-user')
-export class WorkspaceUserController {
-  constructor(private readonly workspaceUserService: WorkspaceUserService) {}
-}
+export class WorkspaceUserController {}
--- src/modules/workspaces/workspaces.service.ts
+++ src/modules/workspaces/workspaces.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { WorkspaceEntity } from './entities/workspace.entity';
-import { WorkspaceExternalData } from '@app/common/interfaces';
-import { buildDataMapByAttribute } from '@app/helpers/buildDataMapByAttribute';
-import { classifyMapDifferences, persistEntityChanges } from '@app/common/utils';
 
 @Injectable()
 export class WorkspacesService {
@@ -27,37 +24,5 @@ export class WorkspacesService {
 
   async remove(id: string): Promise<void> {
     await this.workspaceRepository.delete(id);
-  }
-
-  async createOrUpdateWorkspaces(workspaces: WorkspaceExternalData[]): Promise<void> {
-    if (workspaces.length === 0) return;
-
-    const workspaceIds = workspaces.map((w) => w.id);
-    const existingWorkspaces = await this.workspaceRepository.findBy({ id: In(workspaceIds) });
-
-    const externalMap = buildDataMapByAttribute(workspaces);
-    const dbMap = buildDataMapByAttribute(existingWorkspaces);
-
-    const { toAddOrUpdate, toDelete } = await classifyMapDifferences<WorkspaceExternalData, WorkspaceEntity>(
-      externalMap,
-      dbMap,
-      this.isWorkspaceChanged.bind(this),
-      this.mapWorkspaceDataToWorkspaceEntity.bind(this),
-      this.workspaceRepository.create.bind(this.workspaceRepository)
-    );
-
-    await persistEntityChanges(this.workspaceRepository, toAddOrUpdate, toDelete);
-  }
-
-  // =============================== UTILS ===============================
-
-  private isWorkspaceChanged(workspace: WorkspaceEntity, external: WorkspaceExternalData): boolean {
-    return workspace.name !== external.name;
-  }
-
-  private async mapWorkspaceDataToWorkspaceEntity(external: WorkspaceExternalData): Promise<Partial<WorkspaceEntity>> {
-    return {
-      name: external.name,
-    };
   }
 }
